15be3431ac61326818b2c5a4d1151c67e5bbab35
Yu Sato <ym.74.mn@gmail.com>
2021-06-04 20:59:46 +0900

    [Start]FizzBuzz

---
+++ FizzBuzz/FizzBuzz.java
@@ -0,0 +1,20 @@
+public class FizzBuzz {
+  // 1から順番に数を表示する
+  // その数が３で割り切れるなら”Fizz”、
+  // ５で割り切れるなら”Buzz”、
+  // 両方で割り切れるなら”FizzBuzz”と表示する。
+  public static void main(String[] args) {
+    // 変数宣言
+    // ループカウンタ
+    int i = 0;
+    // 繰り返し最大数
+    int loopCount = 100;
+    // ３の倍数
+    int numFizz = 3;
+    // ５の倍数
+    int numBuzz = 5;
+    String strFizz = "Fizz";
+    String strBuzz = "Buzz";
+
+  }
+}
