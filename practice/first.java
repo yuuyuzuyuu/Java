07217f0c305cbe1070f4460edadf214078a75e9f
Yu Sato <ym.74.mn@gmail.com>
2021-06-10 23:05:54 +0900

    [Add]practice

--- practice/first.java
+++ practice/first.java
@@ -1,6 +1,9 @@
 public class first {
   public static void main(String[] args) {
-     System.out.println("Hello World");
-
+     int x = 7;
+     x *= 3;
+     System.out.println(x);
+     x /= 2;
+     System.out.println(x);
   }
 }
