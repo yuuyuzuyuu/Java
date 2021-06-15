fc0218cd1c4b2872455a4b2829c916a3bd59a38a
Yu Sato <ym.74.mn@gmail.com>
2021-06-15 16:16:50 +0900

    [Add]practice

--- test/src/main05.java
+++ test/src/main05.java
@@ -1,15 +1,26 @@
 
 public class main05 {
 	public static void main(String[] args) {
-		int size = 3;
-		for (int i = 0; i < size; i++) {
-			for(int j = 0; j < size-(i+1); j++) {
+		int number = 3;
+		for(int i = 0; i < number; i ++) {
+			for(int s = 0; s < number-(i+1); s++) {
 				System.out.print(" ");
 			}
-			for(int k = 0; k<(i+1)*2-1; k++) {
+			for(int k = 0; k< (i+1)*2-1;k++) {
 				System.out.print("*");
 			}
-			System.out.print("\n");
+			System.out.println();
+		}
+		
+		int size = 3;
+		for (int a = 0; a < size; a++) {
+			for (int b = 0; b < a; b++) {
+				System.out.print(" ");
+			}
+			for (int c = 0; c < (size-a)*2-1;c++) {
+				System.out.print("*");
+			}
+			System.out.println();
 		}
 	}
 }
---
+++ test/src/test03.java
@@ -0,0 +1,8 @@
+
+public class test03 {
+	public static void main(String[] args) {
+		for(int i = 1; i <= 9; i++) {
+			System.out.println(i*4);
+		}
+	}
+}
